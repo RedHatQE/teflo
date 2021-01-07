833c4e55262268c9bab53ba8ad8097b8e3fe30f2
Rujuta Shinde <rushinde@redhat.com>
2021-01-07 14:54:52 -0500

    Bump version: 0.2.1 → 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2020, Red Hat Inc.'
 author = u'Red Hat Inc.'
 
 # The short X.Y version
-version = u'0.2.1'
+version = u'1.0.0'
 # The full version, including alpha/beta/rc tags
-release = u'0.2.1'
+release = u'1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
