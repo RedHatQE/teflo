6c491f769fe4e8aec4f4eb8eacc65fb3af64b448
JunqiZhang0 <junqzhan@redhat.com>
2021-01-21 13:27:44 -0500

    test github action 3

--- .github/deployTestingImage.sh
+++ .github/deployTestingImage.sh
@@ -2,11 +2,13 @@ base_container=${1}
 
 sed "s/BaseContainerImage/${base_container}/" ./dockerfile/Dockerfile -i
 
+QUAY_USERNAME=${2}
+QUAY_PASSWORD=${3}
 #yum install podman -y
 name_prefix=$(echo ${base_container} | sed -e "s/://g")
-podman login quay.io --username ${ secrets.QUAY_USERNAME } --password ${ secrets.QUAY_PASSWORD  }
+podman login quay.io --username ${QUAY_USERNAME} --password ${QUAY_PASSWORD}
 podman build ./dockerfile/ -t ${name_prefix}-testing-teflo
 podman tag ${name_prefix}-testing-teflo quay.io/junqizhang0/${name_prefix}-testing-teflo
 podman push quay.io/junqizhang0/${name_prefix}-testing-teflo
-sed "s/${name_prefix}/BaseContainerImage/" ./dockerfile/Dockerfile -i
+sed "s/${base_container}/BaseContainerImage/" ./dockerfile/Dockerfile -i
 
