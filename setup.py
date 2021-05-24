1b09cec9af828cb6fc7476074c2baa46ba6ba2e0
Rujuta Shinde <rushinde@redhat.com>
2021-05-24 14:22:32 -0400

    CCITCARBON-313 Unable to install Ansible collections with ansible 4.0

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
     zip_safe=False,
     install_requires=[
         'ansible>=2.5.0',
-        'ansible-base != 2.10.6',
         'apache-libcloud==2.2.0',
         "blaster>=0.3.0",
         'Click>=6.7',
