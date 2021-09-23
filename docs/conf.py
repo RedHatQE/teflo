9d159f0889c3137f9fdbfb11b87a488293d115fc
JunqiZhang0 <junqzhan@redhat.com>
2021-09-23 18:57:27 -0400

    Bump version: 1.2.3 → 1.2.4

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2020, Red Hat Inc.'
 author = u'Red Hat Inc.'
 
 # The short X.Y version
-version = u'1.2.3'
+version = u'1.2.4'
 # The full version, including alpha/beta/rc tags
-release = u'1.2.3'
+release = u'1.2.4'
 
 
 # -- General configuration ---------------------------------------------------
