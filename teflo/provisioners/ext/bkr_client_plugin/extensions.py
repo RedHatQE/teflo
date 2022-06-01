54d1d83285d9c7955621f170d325d005ab031340
shay <shshevac@redhat.com>
2022-06-01 18:00:37 +0300

    strict validation to bkr_client schema (#227)

---
+++ teflo/provisioners/ext/bkr_client_plugin/extensions.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Red Hat, Inc.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+    Pykwalify extensions module for beaker client plugin
+
+    Module containing custom validation functions used for beaker client schema checking.
+
+    :copyright: (c) 2021 Red Hat, Inc.
+    :license: GPLv3, see LICENSE for more details.
+"""
+
+import os
+
+
+def valid_file_exist(value, rule_obj, path):
+    """ Verify if the given file exist."""
+
+    if os.path.exists(value):
+        return True
+    else:
+        raise AssertionError(
+            '%s must be an existing file! Cannot find file: %s.' % (path.split('/')[-1], value)
+        )
