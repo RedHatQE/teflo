f9d4f667f7c05a1a326d061ec0a0303a02291057
Guy Yaakov <gyaakov@redhat.com>
2022-11-07 17:28:45 +0200

    Bump version: 2.2.7 → 2.2.8

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2022, Red Hat Inc.'
 author = u'Red Hat Inc.'
 
 # The short X.Y version
-version = u'2.2.7'
+version = u'2.2.8'
 # The full version, including alpha/beta/rc tags
-release = u'2.2.7'
+release = u'2.2.8'
 
 
 # -- General configuration ---------------------------------------------------
